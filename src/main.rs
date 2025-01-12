92c5d2c621b64d3d71fe9598b658d2fb81876566
Ian Deans <ian@ideans.dev>
2025-01-11 20:42:46 -0800

    update from ian
    pushed stuff

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,16 @@ fn main() {
     // startup
     println!("Hello, world!");
 
-    // main loop
+    let mut i:i32 = 0;    
 
+    // main loop
+    loop {
+        i = i+1;
+        if i>20 {
+            break;
+        }
+
+    }
 
     // shutdown
     

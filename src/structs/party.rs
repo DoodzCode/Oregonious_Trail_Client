f958dfc21c257e3b5a9722b8e1032d065ca20d2d
Ian Deans <ian@ideans.dev>
2025-01-18 13:27:57 -0800

    combined efforts

--- src/structs/party.rs
+++ src/structs/party.rs
@@ -2,7 +2,7 @@
 #[derive(Debug)]
 pub struct Party {
   pub name: String,
-  pub position: u16, 
+  position: u16, 
   pub head_count: u16,
 }
 
@@ -14,7 +14,32 @@ impl Party {
       head_count: 140,
     }
   }
-  pub fn 
+  
+  pub fn increment_position(&self, distance: u16 ) -> u16 {
+    self.position + distance
+  }
+
+  pub fn decrement_position(&self, distance: u16) -> u16 {
+    self.position - distance
+  }
+
+  pub fn increment_head_count(&self, amount: u16 ) -> u16 {
+    self.head_count + amount
+  }
+
+  pub fn decrement_head_count(&self, amount: u16) -> u16 {
+    self.head_count - amount
+  }
+
+  pub fn give_position(&self) -> &u16 {
+    &self.position
+  }
+
+
 }
+
+
+
+
 
 

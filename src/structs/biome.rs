56e65d1036fad5371f719547123358e8165077d6
Ian Deans <131498335+Fryymann@users.noreply.github.com>
2025-01-29 20:30:01 -0800

    add biome stuff

--- src/structs/biome.rs
+++ src/structs/biome.rs
@@ -4,10 +4,32 @@ use serde::{Serialize, Deserialize};
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Biome {
     pub name: String,
+    pub b_type: BiomeType,
 }
 
 impl fmt::Display for Biome {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Biome.name: {} \n", self.name)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum BiomeType {
+    Grasslands,
+    Forest,
+    Desert,
+    Mountain,
+    Canyon,
+}
+
+impl BiomeType {
+    pub fn base_chance_of_rain(&self) -> u8 {
+        match self {
+            BiomeType::Grasslands => 50,
+            BiomeType::Forest => 60,
+            BiomeType::Desert => 10,
+            BiomeType::Mountain => 40,
+            BiomeType::Canyon => 30,
+        }
     }
 }

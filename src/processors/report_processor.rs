b880c93307c0b5255407e1fe256c5bcfa676103a
ndeans <ndeans@hotmail.com>
2025-01-29 18:39:20 -0800

    it's game_STATE!

--- src/processors/report_processor.rs
+++ src/processors/report_processor.rs
@@ -2,9 +2,9 @@ use std::fmt::Debug;
 
 use crate::structs::game_state::GameState;
 
-pub fn status_report(game_data: &mut GameState) {
+pub fn status_report(game_state: &mut GameState) {
     println!("Status Report: ");
-    println!("{}", game_data);
+    println!("{}", game_state);
 
     println!();
     println!();

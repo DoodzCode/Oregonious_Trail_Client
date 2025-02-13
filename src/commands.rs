017d1fb94f8e4eec1a427d312c82c7b99774f2df
Ian Deans <131498335+Fryymann@users.noreply.github.com>
2025-02-12 21:25:39 -0800

    get commands to work in a hacky way

--- src/commands.rs
+++ src/commands.rs
@@ -1,9 +1,21 @@
 use std::net::TcpStream;
 use std::io::Write;
 
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Command {
+  name: String,
+  arguments: Vec<String>,
+}
+
 pub fn say(cmd: (&str, Vec<&str>), tcp_stream: &mut TcpStream) {
-  let msg = cmd.1.join(" ");
-  let msg = format!("say {}", msg);
+  let cmd  = Command {
+    name: cmd.0.to_string(),
+    arguments: cmd.1.iter().map(|s| s.to_string()).collect(),
+  };
+  let msg = serde_json::to_string(&cmd).unwrap();
+
   tcp_stream.write(msg.as_bytes()).unwrap();
 
 }
